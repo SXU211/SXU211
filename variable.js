d4e6a621659a0791f75f91d788c5a4c229735be2
SXU211 <suhailuddin1231070@gmail.com>
2026-06-30 15:27:42 +0100

    variable types

--- variable.js
+++ variable.js
@@ -1,6 +1,10 @@
 var firstName = "suhail";
 var age = 15;
 var isMale = true;
+var balance = 100.32;
+var dob = new Date(2011,4,31);
+var person = {};
+var empty = undefined;
 
 
 
@@ -9,6 +13,10 @@ var isMale = true;
 
 
 
-console.log(firstName)
-console.log(age) 
-console.log(isMale)
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMale);
+console.log(typeof balance);
+console.log(typeof dob);
+console.log(typeof person)
+console.log(typeof empty)
